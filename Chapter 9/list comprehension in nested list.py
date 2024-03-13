bc49a8e8df470aaa70783f00772443c5686e84e5
DarshanPatil02 <dp07018@gmail.com>
2024-03-13 18:31:31 +0530

    list comprehension

---
+++ Chapter 9/list comprehension in nested list.py
@@ -0,0 +1,5 @@
+# list comprehension in nested list
+# get ouput as ---> example = [[1,2,3],[1,2,3],[1,2,3]]
+
+nested_comp = [[i for i in range(1,4)] for j in range(3)]
+print(nested_comp)
--- Chapter 9/list_comprehension with if statement.py
+++ Chapter 9/list_comprehension with if statement.py
@@ -7,3 +7,10 @@ print(even_numbers)
 
 odd_nums = [i for i in range(1,11) if i%2!=0]
 print(odd_nums)
+
+# list comprehension if else
+# get an output from a list if number inside list is even make it double and if number is odd make it negative
+
+ls = list(range(1,11))
+output = [i*2 if i%2==0 else -i for i in ls]
+print(output)
