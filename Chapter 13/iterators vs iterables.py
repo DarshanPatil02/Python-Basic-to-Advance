521506b82b0c3190d31c86d9a1b3acbd6836a103
DarshanPatil02 <dp07018@gmail.com>
2024-03-19 16:48:55 +0530

    Iterators and Iterables

--- Chapter 13/iterators vs iterables.py
+++ Chapter 13/iterators vs iterables.py
@@ -1,6 +1,10 @@
-numbers = [1,2,3,4]
-squares=list(map(lambda a:a**2, numbers))
+numbers = [1,2,3,4] # iterables
+squares=list(map(lambda a:a**2, numbers)) # iterators
 print(squares)
 
 for i in squares:
     print(i)
+
+iter_number = iter(numbers)
+print(list(iter_number))
+print(next(iter_number))
