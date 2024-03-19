b0e9370ca8145ebada2ea05473f74934a88370dd
DarshanPatil02 <dp07018@gmail.com>
2024-03-19 19:37:28 +0530

    Sorted function

---
+++ Chapter 13/sorted function.py
@@ -0,0 +1,8 @@
+# sorted function
+
+guitars = {
+    {'model':'yamaha f310', 'price':8400},
+    {'model':'faith naptune', 'price':50000},
+    {'model':'faith apollo venus', 'price':35000},
+    {'model':'taylor 814ce', 'price':450000}
+}
