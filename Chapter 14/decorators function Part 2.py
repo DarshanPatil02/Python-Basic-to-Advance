36ecae8af4410bf202651857176e7e20af0553df
DarshanPatil02 <dp07018@gmail.com>
2024-03-21 10:16:01 +0530

    Decorator function

---
+++ Chapter 14/decorators function Part 2.py
@@ -0,0 +1,39 @@
+# In part 1 if we pass follwing argument then it show error
+
+def decorator_function(any_function):
+    def wrapper_function():
+        print("This is awesome function")
+        any_function()
+    return wrapper_function
+
+@decorator_function
+def function1():
+    print(f"This is function 1")
+function1()
+
+
+# But if pass following command then function will show error
+
+def decorator_function(any_function):
+    def wrapper_function():
+        print("This is awesome function")
+        any_function()
+    return wrapper_function
+
+@decorator_function
+def function1(num):
+    print(f"This is function 1 with number {num}")
+function1(3)
+
+
+# Then follow this syntax
+def decorator_function(any_function):
+    def wrapper_function(*args,**kwargs): # by adding *args and **kwargs
+        print("This is awesome function")
+        return any_function(*args,**kwargs) # Here also same changes and also return the function
+    return wrapper_function
+
+@decorator_function
+def function1(num):
+    print(f"This is function 1 with number {num}")
+function1(3)
