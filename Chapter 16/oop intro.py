e039d0d9aac7fb3fc39dc9b82f2bbffa37e7a094
DarshanPatil02 <dp07018@gmail.com>
2024-03-21 16:42:44 +0530

    Object Oriented Programming

---
+++ Chapter 16/oop intro.py
@@ -0,0 +1,34 @@
+# OBJECT ORIENTED PROGRAMMING
+
+# COMMON TOPIC IN ALMOST ALL POPULAR PROGRAMMING LANGUAGES (PYTHON, C++, JAVA)
+# WITH COMMON IDEA WITH DIFFERENT SYNTAX 
+
+# OOP IS JUST A STYLE TO WRITE CODE
+
+# VERY HELPFUL IN CREATING REAL WORLD PROGRAMS
+
+# Most common word used in oop are class,object(instance), method
+# e.g.
+
+# Class : --->
+#Define a class, which is like a blueprint for creating an object Use classes to create new objects
+# list is a class which is in-built in python.
+
+# Object (Instance) --->
+"""
+While the class is the blueprint, an instance is an object that's built from a class 
+and contains real data like in above case list is class and l,l1,l2,stc are objects
+e.g:--> l, l1, l2,etc are objects or instances
+"""
+
+l = [1,2,3,4,5]   
+l1 = [5,8,9,58] 
+l2 = ["Orange","mango","apple"]
+
+# Method --->
+"""Classes define functions called methods, 
+which identify the behaviors and actions that an object created from the class can perform with its data.
+e.g: - append, delete, insert, pop, etc
+"""
+l.append(6)
+l.pop(2)
