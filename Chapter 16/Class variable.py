8eecb05ecfe63a547f25bd6facd62e45cda1078f
DarshanPatil02 <dp07018@gmail.com>
2024-03-21 18:17:26 +0530

    Exercise 02

--- Chapter 16/Class variable.py
+++ Chapter 16/Class variable.py
@@ -18,3 +18,18 @@ print(c1.radius)
 print(c1.area())
 print(c1.circumference())
 
+# Create a class Person as you want 
+# And tell how many instance it has
+
+class Person:
+    count_instance = 0
+    def __init__(self,f_name,l_name,age):
+        Person.count_instance+=1
+        self.f_name = f_name
+        self.l_name = l_name
+        self.age = age
+
+p1 = Person("Rohit","Sharma",42)
+p2 = Person("Surya","Yadav",38)
+
+print(Person.count_instance)
