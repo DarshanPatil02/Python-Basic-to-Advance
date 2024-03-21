6d017c36841d1b55714758c492570eeeff71ad54
DarshanPatil02 <dp07018@gmail.com>
2024-03-21 12:28:42 +0530

    Decorator function practice

--- Chapter 14/decorators practice.py
+++ Chapter 14/decorators practice.py
@@ -25,3 +25,53 @@ def multiply(a,b):
     return a*b
 
 print(multiply(4,3))
+
+# Decorator practice
+#case 1
+def add_all(*args):
+    total=0
+    for i in args:
+        total+=i
+    return total
+print(add_all(1,2,3,4,5,6)) # It is not showing error but if
+print(add_all(1,2,3,4,5,6,[5,6,8])) # It is showing error
+
+# So create a decorator function that only takes int datatype
+
+def int_only(function):
+    def wrapper_func(*args,**kwargs):
+        data_type = []
+        for arg in args:
+            data_type.append(type(arg)==int)
+        if all(data_type):
+                return function(*args,**kwargs)
+        else:
+            print("Invalid arguement")
+    return wrapper_func
+
+@int_only
+def add_all(*args):
+    total=0
+    for i in args:
+        total+=i
+    return total
+print(add_all(1,2,3,4,5,6)) # It is not showing error but if
+print(add_all(1,2,3,4,5,6,[5,6,8])) # It is showing error
+
+# OR 
+def int_only(function):
+    def wrapper_func(*args,**kwargs):
+        if all([type(arg)==int for arg in args]):
+                return function(*args,**kwargs)
+        print("Invalid arguement")
+    return wrapper_func
+
+@int_only
+def add_all(*args):
+    total=0
+    for i in args:
+        total+=i
+    return total
+print(add_all(1,2,3,4,5,6)) # It is not showing error but if
+print(add_all(1,2,3,4,5,6,[5,6,8])) # It is showing error
+
