acb1f550d14e6411634e8094b1c6735816793746
DarshanPatil02 <dp07018@gmail.com>
2024-03-21 17:09:39 +0530

    Exercise on OOP

--- Chapter 16/Exercise.py
+++ Chapter 16/Exercise.py
@@ -8,6 +8,23 @@ class Laptop:
         self.price = price
         self.laptop_name = brand_name+" "+model_name
 
+
+laptop1 = Laptop("HP","au114tx", 63000)
+laptop2 = Laptop("Dell","bt185jp", 85000)
+
+print(laptop1.model_name)
+print(laptop2.price)
+print(laptop1.laptop_name)
+
+# OR  (Insted of self used laptop)
+class Laptop:
+    def __init__(laptop,brand_name,model_name,price):
+        laptop.brand_name = brand_name
+        laptop.model_name = model_name
+        laptop.price = price
+        laptop.laptop_name = brand_name+" "+model_name
+        
+
 laptop1 = Laptop("HP","au114tx", 63000)
 laptop2 = Laptop("Dell","bt185jp", 85000)
 
