68c8d39cf5b4d523cb40703face59ab73fba2940
DarshanPatil02 <dp07018@gmail.com>
2024-03-22 11:09:48 +0530

    Multiple  inheritance

--- Chapter 16/multiple inheritance.py
+++ Chapter 16/multiple inheritance.py
@@ -8,8 +8,17 @@ class A:
         return "Hello from class A"
         
 class B:
-    def class_a_method(self):
+    def class_b_method(self):
         return "I'am just a class B method"
     
     def hello(self):
         return "Hello from class B"
+    
+class C(A,B):
+    pass
+
+isinstance_c = C()
+print(isinstance_c.class_a_method())
+print(isinstance_c.class_b_method()) 
+print(isinstance_c.hello())  # here class A and Class B also has hello function but as per method resolution it will A
+print(help(isinstance_c))
