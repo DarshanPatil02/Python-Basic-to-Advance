517a3018d15967ed9001e1e8ea6dc4025bb4400b
DarshanPatil02 <dp07018@gmail.com>
2024-03-22 17:24:55 +0530

    Custom exception handling

---
+++ Chapter 17/custom exception.py
@@ -0,0 +1,26 @@
+# python custom exceptions
+# Q - Why custom exceptions
+# A - To increase readability of code
+'''
+# Example
+
+def validate(name):
+    if len(name)<8:
+        raise ValueError("Name is too short")
+    
+username="Darshan"
+validate(username)
+print(f"hello {username}")
+'''
+# Instead of ValueError we want other like NametooshortError
+
+class NametooshortError(ValueError):
+    pass
+
+def validate(name):
+    if len(name)<8:
+        raise NametooshortError("Name is too short") # here we have to define NametooshortError class first
+
+username=input("Enter your name: ")
+validate(username)
+print(f"hello {username}")
