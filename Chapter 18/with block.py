133cb9b6f4c54b86dfc2693676af4af090db4cc6
DarshanPatil02 <dp07018@gmail.com>
2024-03-22 19:55:51 +0530

    With block for file oper.

--- Chapter 18/with block.py
+++ Chapter 18/with block.py
@@ -1,5 +1,5 @@
 # with block it not only used for reading or open it is used for many purpose
-# with block is also called context manager
+# with block is also called context manager...
 
 with open("Chapter 18\Good thoughts.txt") as f:
     for line in f:
